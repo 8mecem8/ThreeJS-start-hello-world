e0ca2f994108c1b6342daaec0dc3f80ef2ca1c32
Cem  Onal <42888722+8mecem8@users.noreply.github.com>
2021-03-03 18:17:11 -0500

    updated

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -23,9 +23,69 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () => {
+    //Update sizes
+    sizes.width = window.innerWidth
+    sizes.height =window.innerHeight
+    //Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    //Update Renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+
+window.addEventListener('dblclick', () => 
+{
+
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+        if(!fullscreenElement)
+        {
+            if(canvas.requestFullscreen)
+             {
+            canvas.requestFullscreen()
+             }
+        else if(canvas.webkitRequestFullscreen)
+             {
+            canvas.webkitRequestFullscreen()
+            }
+        }
+        else
+        {
+             if(document.exitFullscreen)
+              {
+            document.exitFullscreen()
+               }
+        else if(document.webkitExitFullscreen)
+              {
+            document.webkitExitFullscreen()
+              }
+        }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * Camera
@@ -46,6 +106,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
